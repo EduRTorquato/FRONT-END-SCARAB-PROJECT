f5cab17e9bb3bf82ada2ae8472c39beec6447461
EduRTorquato <eduardotorquato78@gmail.com>
2024-09-04 17:49:32 -0300

    inputs IDs e e Inputs Maks

--- pi 4 semestre/js/cadastro.js
+++ pi 4 semestre/js/cadastro.js
@@ -1,13 +1,35 @@
+$('#cpf').mask('000.000.000-00');
+
+
 const btnEntrar = document.getElementById("btnEntrar");
+const cpf = document.getElementById("cpf");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const confirmSenha = document.getElementById("confirmSenha");
+const checkBox = document.getElementById("checkBox");
+
+
+
+
 
 
 btnEntrar.addEventListener("click", function () {
     console.log("Hello!");
-    Swal.fire({
-        position: "top-end",
-        title: 'Preencha todos os campos!',
-        showConfirmButton: false,
-        icon: 'error',
-        timer: 1500
+
+    console.log({
+        cpf: cpf.value,
+        email: email.value,
+        senha: senha.value,
+        confirmSenha: confirmSenha.value,
+        checkBox: checkBox.value
     })
+
 });
+
+// Swal.fire({
+//     position: "top-end",
+//     title: 'Preencha todos os campos!',
+//     showConfirmButton: false,
+//     icon: 'error',
+//     timer: 1500
+// })
