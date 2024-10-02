460b741226ac940ad8ad421ebf5df957fa7c0e96
EduRTorquato <eduardotorquato78@gmail.com>
2024-10-02 18:07:25 -0300

    front-download-images

--- pi 4 semestre/js/home.js
+++ pi 4 semestre/js/home.js
@@ -21,15 +21,18 @@ async function chamarProdutos() {
     });
 }
 
-function verificaPrincipal(array){
-    
-    return array.filter();
+function verificaPrincipal(array) {
+    const principalItem = array.find(function(dado) {
+        return dado.principal;
+    });
+
+    return principalItem ? principalItem.caminho : "../IMAGENS/imgUpload/noimg.jpg";
 }
 
 function criaCardsProdutos(data){
 
     data.forEach(element => {
-        console.log(1 + 1);
+
 
         var card = document.createElement("div");
         var image = document.createElement("img");
@@ -37,9 +40,52 @@ function criaCardsProdutos(data){
         var preco = document.createElement("p");
         var link = document.createElement('a');
         var button = document.createElement("button");
+        var span  = document.createElement("span");
 
 
-        verificaPrincipal(data.imagens);
+        console.log(verificaPrincipal(element.imagens));
+
+        card.classList.add("card");
+        nomeProduto.classList.add("nomeProduto");
+        preco.classList.add("preco");
+        button.classList.add("btn");
+        button.classList.add("btn-light");
+        span.classList.add("star");
+        
+
+        containerCard.appendChild(card);
+        card.appendChild(span);
+        card.appendChild(image);
+        card.appendChild(nomeProduto);
+        card.appendChild(preco);
+        card.appendChild(link);
+        link.appendChild(button);
+
+        if(element.avaliacao >= 1 && element.avaliacao < 2 ){
+            span.innerHTML ="★";
+        }
+        else if(element.avaliacao >= 2 && element.avaliacao < 3 ){
+            span.innerHTML ="★ ★";
+        }
+        else if(element.avaliacao >= 3 && element.avaliacao < 4 ){
+            span.innerHTML ="★ ★ ★ ";
+        }
+        else if(element.avaliacao >= 4 && element.avaliacao < 5){
+            span.innerHTML ="★ ★ ★ ★ ";
+        }
+        else{
+            span.innerHTML ="★ ★ ★ ★ ★";
+        }
+        
+        
+        image.src = verificaPrincipal(element.imagens);
+        nomeProduto.innerHTML = element.nome;
+        preco.innerHTML = element.preco;
+        button.innerHTML = "Comprar"
+
+      
+
+
     });
 
 
